e5cb88c01ef11b7005ca9b5509202ecbf9e95a92
Mansi Singh <mansisingh@Mansis-MacBook-Air.local>
2021-09-09 14:58:45 -0500

    new

---
+++ build/precache-manifest.99a6aad56740b9a29ae14a62162a0571.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c212aed2f77d6e75f45286d06d59b383",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/index.html"
+  },
+  {
+    "revision": "f18850a3a9d16a374df0",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/css/main.fd355fee.chunk.css"
+  },
+  {
+    "revision": "acd4acc627a3a17e6f7b",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/js/2.d0f445ad.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/js/2.d0f445ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18850a3a9d16a374df0",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/js/main.0ac0fa91.chunk.js"
+  },
+  {
+    "revision": "d43bf22da9af95be53aa",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/js/runtime-main.6c1fe1dc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c85453730efa2ebe9e71e64b8a7e7b68",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/amazon.c8545373.jpg"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "3952f41bd410b0f9b33c3242dc17ccda",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/coursera.3952f41b.png"
+  },
+  {
+    "revision": "cb91d56325b069fa29ca8746c05c8ac7",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/cra-logo.cb91d563.jpg"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "10576d72da4da1aa04f5e9e35b4a328a",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/dal.10576d72.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "3d9465c3483dffb591b31f101699d0dc",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/gtu.3d9465c3.png"
+  },
+  {
+    "revision": "7a2dbf7a2595600c00118eea8a7a52f8",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/hackathon1.7a2dbf7a.svg"
+  },
+  {
+    "revision": "0fce03eea24aef3649114ce63a4335a1",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/hardik.0fce03ee.jpeg"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "b7f903d747b0409347c9ee67745dbfa4",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/localguide.b7f903d7.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "5dcd18d03068fbd9edeeb5003b21998a",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/nightowls.5dcd18d0.jpg"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "7b7a76ef55658f6999ce00c5d55f85a5",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/skillenza.7b7a76ef.png"
+  },
+  {
+    "revision": "9f38217ab187dddce5462149e5dcb991",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/udemy.9f38217a.png"
+  },
+  {
+    "revision": "246ad3ede6a55b0102afa9871cfcbf21",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/valora.246ad3ed.jpg"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "/%E2%80%9Chomepage%E2%80%9D:%20%E2%80%9Chttp://mansi10.github.io/mansi-portfolio/static/media/wrighter.02fdae43.jpg"
+  }
+]);
